1f814e83492eceb489f9f28af472d023671b82a8
Shree <Shree@DESKTOP-CALVDQI>
2023-12-13 23:56:37 +0530

    after branch

--- AgileCrm/src/test/java/day13dec23/Demo.java
+++ AgileCrm/src/test/java/day13dec23/Demo.java
@@ -5,6 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("First Edit at Central repo");
 		System.out.println("Again edit in local master repo");
+		int a=10;
+		int b=19;
+		System.out.println(a+b);
 
 	}
 
